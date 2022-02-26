542759e16f92352988c9a0f13eeb2e7ec07c99a8
ebajaruns <bajaruns@gmail.com>
2022-02-26 10:02:17 +0200

    bilde

--- speles_faili/EdgarsB/testa_spele.py
+++ speles_faili/EdgarsB/testa_spele.py
@@ -4,9 +4,18 @@ import random
 canvas_width = 600
 canvas_height = 600
 master = Tk()
+
+
 logs = Canvas(master, 
            width=canvas_width, 
            height=canvas_height)
 master.title("Līnijas spēle")
+logs.pack()
+
+
+sarkG = PhotoImage(file="speles_faili\EdgarsB\sark_maza.ppm")
+#logs.create_image(250,250, image= sarkG)
+
+player = logs.create_image(250,250, image = sarkG)
 
 master.mainloop()
