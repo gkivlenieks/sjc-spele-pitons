639d78f82d168eeea47486657168acc343c7f804
ebajaruns <bajaruns@gmail.com>
2022-02-26 10:06:59 +0200

    izmainas un notikumi

--- speles_faili/EdgarsB/testa_spele.py
+++ speles_faili/EdgarsB/testa_spele.py
@@ -5,17 +5,18 @@ canvas_width = 600
 canvas_height = 600
 master = Tk()
 
-
+#Izveidojam spelēs laukumu!!! neaizmirstam komandu .pack()
 logs = Canvas(master, 
            width=canvas_width, 
            height=canvas_height)
 master.title("Līnijas spēle")
 logs.pack()
 
-
-sarkG = PhotoImage(file="speles_faili\EdgarsB\sark_maza.ppm")
+#Izveidojam spēlētāju (bilde)
+sarkG = PhotoImage(file="speles_faili\EdgarsB\sark_videja.ppm")
 #logs.create_image(250,250, image= sarkG)
-
 player = logs.create_image(250,250, image = sarkG)
 
+
+#Master mainloops - izmaiņas un notikumi
 master.mainloop()
