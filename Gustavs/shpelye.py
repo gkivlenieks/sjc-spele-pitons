06a8a0c00f08908d890f9b9f107230eeb1a1e10a
gkivlenieks <kivlenieksgustavs@gmail.com>
2022-02-26 09:59:16 +0200

    n

---
+++ Gustavs/shpelye.py
@@ -0,0 +1,19 @@
+from tkinter import *
+import random
+
+master = Tk()
+
+CW = 600
+CH = 600
+
+logs = Canvas (
+    master,
+    width=CW,
+    height=CH
+)
+
+master.title("Linijas spēle")
+
+
+
+master.mainloop()
