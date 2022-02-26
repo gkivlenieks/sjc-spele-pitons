75b3f7a181ab2dba69a7b0c99292871c924683be
gkivlenieks <kivlenieksgustavs@gmail.com>
2022-02-26 10:35:56 +0200

    marijo gets to move

--- Gustavs/shpelye.py
+++ Gustavs/shpelye.py
@@ -1,38 +1,56 @@
 from email.mime import image
 from tkinter import *
-import random
+from PIL import Image, ImageTk, ImageOps
 
 master = Tk()
 
 CW = 600
 CH = 600
 pstep = 5
+grass = PhotoImage(file='Gustavs/assets/graaas.ppm')
 
 logs = Canvas (
     master,
     width=CW,
     height=CH
 )
+bukgronds = logs.create_image(CW // 2, CH // 2, image= grass)
 logs.pack()
-ma = PhotoImage(file="Gustavs/assets/marijo.ppm")
+marijo = Image.open('Gustavs/assets/marijo.ppm')
+ma = ImageTk.PhotoImage(marijo)
 playah = logs.create_image(CW // 2, CH // 2, image = ma)
 master.title("Linijas spēle")
 px = CW // 2
 py = CH // 2
 master.resizable(False, False)
+m = True
 
 def playahmove(way):
-    global px, py, playah
+    global px, py, playah, marijo, ma, m
+    w = False
     if way == 'right':
+        mm = True
         px += pstep
+        w = True
     elif way == 'left':
+        mm = False
         px -= pstep
+        w = True
     elif way == 'up':
         py -= pstep
     elif way == 'down':
         py += pstep
     logs.delete(playah)
+    if w and mm == False and m == True:
+        m = False
+        marijo = ImageOps.mirror(marijo)
+        ma = ImageTk.PhotoImage(marijo)
+    elif w and mm and m == False:
+        m = True
+        marijo = ImageOps.mirror(marijo)
+        ma = ImageTk.PhotoImage(marijo)
     playah = logs.create_image(px, py, image = ma)
+    w = False
 
 
 def on_keypress(event):
