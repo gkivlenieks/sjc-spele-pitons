feeee88b2f884e02177387a0fb271992aa86a63f
ebajaruns <bajaruns@gmail.com>
2022-02-26 10:34:25 +0200

    bildes

--- speles_faili/EdgarsB/testa_spele.py
+++ speles_faili/EdgarsB/testa_spele.py
@@ -1,8 +1,8 @@
 from calendar import c
 from tkinter import *
 import random
-canvas_width = 600
-canvas_height = 600
+canvas_width = 900
+canvas_height = 900
 master = Tk()
 
 #Izveidojam spelēs laukumu!!! neaizmirstam komandu .pack()
@@ -16,6 +16,11 @@ logs.pack()
 sarkG = PhotoImage(file="speles_faili\EdgarsB\sark_videja.ppm")
 #logs.create_image(250,250, image= sarkG)
 player = logs.create_image(250,250, image = sarkG)
+
+#Fona attēla iestatīšana... (svarīgs izmērs - der PNG)
+fons = PhotoImage(file="speles_faili\EdgarsB\mezs_sss.png")
+logs.create_image(0,0, image=fons)
+
 
 
 #Master mainloops - izmaiņas un notikumi
