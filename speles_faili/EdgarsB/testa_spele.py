b594e4d346b77e8c017f78d6a4c09ff81d61286e
ebajaruns <bajaruns@gmail.com>
2022-02-26 09:49:30 +0200

    sp1

---
+++ speles_faili/EdgarsB/testa_spele.py
@@ -0,0 +1,12 @@
+from calendar import c
+from tkinter import *
+import random
+canvas_width = 600
+canvas_height = 600
+master = Tk()
+logs = Canvas(master, 
+           width=canvas_width, 
+           height=canvas_height)
+master.title("Līnijas spēle")
+
+master.mainloop()
