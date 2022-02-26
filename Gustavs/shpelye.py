f4f4762b10dea4e440634c093fff8ca7c77d1b61
gkivlenieks <kivlenieksgustavs@gmail.com>
2022-02-26 10:53:06 +0200

    komentāri!

--- Gustavs/shpelye.py
+++ Gustavs/shpelye.py
@@ -4,47 +4,70 @@ from PIL import Image, ImageTk, ImageOps
 
 master = Tk()
 
+# canvas augstums, platums
 CW = 600
 CH = 600
+# cik pikseļu playeris pārvietojas
 pstep = 5
+# backgrounds
 grass = PhotoImage(file='Gustavs/assets/graaas.ppm')
+# spēlētāja bildes izmēri, pa cik bilde tiks uztaisīta mazāka
 ImgW = 426
 ImgH = 586
 ImgM = 10
-
+#main logs
 logs = Canvas (
     master,
     width=CW,
     height=CH
 )
-bukgronds = logs.create_image(CW // 2, CH // 2, image= grass)
 logs.pack()
+#fons
+bukgronds = logs.create_image(CW // 2, CH // 2, image= grass)
+#mario bildīte
 marijo = Image.open('Gustavs/assets/BMario-NoBG.png')
 marijo = marijo.resize((ImgW // ImgM, ImgH // ImgM))
 ma = ImageTk.PhotoImage(marijo)
+#mario bildītes izveidošana
 playah = logs.create_image(CW // 2, CH // 2, image = ma)
+#loga title
 master.title("Linijas spēle")
+#sākuma spēlētaja atrašanās vieta
 px = CW // 2
 py = CH // 2
+#logs ir/nav resizable
 master.resizable(False, False)
+#vai spēlētājs skatās pa labi
 m = True
-
+#speletāja kustības funkcija
 def playahmove(way):
     global px, py, playah, marijo, ma, m
+    #vai būs jāmirroro bilde
     w = False
+    #pārbauda kurā virziena tas jāpārvieto
     if way == 'right':
+        #vai tam jāskatās pa labi
         mm = True
+        #x koordinātu maiņa
         px += pstep
+        #vai to būs jāmirroro
         w = True
     elif way == 'left':
+        #vai tam jāskatās pa labi
         mm = False
+        #x koordinātu maiņa
         px -= pstep
+        #vai to būs jāmirroro
         w = True
     elif way == 'up':
+        #y koordinātu maiņa
         py -= pstep
     elif way == 'down':
+        #y koordinātu maiņa
         py += pstep
+    #izdzēš iepriekšejo bildīti
     logs.delete(playah)
+    #pārbauda vai to jāmirroro
     if w and mm == False and m == True:
         m = False
         marijo = ImageOps.mirror(marijo)
@@ -53,6 +76,7 @@ def playahmove(way):
         m = True
         marijo = ImageOps.mirror(marijo)
         ma = ImageTk.PhotoImage(marijo)
+    #izveido jaunu bildīti un uzliek w uz False
     playah = logs.create_image(px, py, image = ma)
     w = False
 
