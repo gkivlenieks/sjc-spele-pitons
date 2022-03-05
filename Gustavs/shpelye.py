bb5a6a09edb06a5c439c9d73c3c68c7c141d113c
gkivlenieks <kivlenieksgustavs@gmail.com>
2022-03-05 11:00:38 +0200

    commit on main

--- Gustavs/shpelye.py
+++ Gustavs/shpelye.py
@@ -1,9 +1,11 @@
 from tkinter import *
 from PIL import Image, ImageTk, ImageOps
 import random
+import time
 
 master = Tk()
 punkti = 0
+mushcnt = 100
 
 # canvas augstums, platums
 CW = 600
@@ -43,13 +45,18 @@ mushroom = mushroom.resize((MushSiz[0] // mushM, MushSiz[1] // mushM))
 seene = ImageTk.PhotoImage(mushroom)
 sx = random.randrange(20, CW - 20, 5)
 sy = random.randrange(20, CH - 20, 5)
-sene = logs.create_image(sx, sy, image= seene)
-def mushrum():
-    global sx, sy, sene
-    logs.delete(sene)
+d = {}
+for x in range(1, mushcnt):
     sx = random.randrange(20, CW - 20, 5)
     sy = random.randrange(20, CH - 20, 5)
-    sene = logs.create_image(sx, sy, image= seene)
+    globals()[f"mush{x}x"] = sx
+    globals()[f"mush{x}y"] = sy
+    globals()[f"mush{x}"] = logs.create_image(sx, sy, image= seene)
+
+def mushrum(m):
+    globals()[f"mush{m}x"] = random.randrange(20, CW - 20, 5)
+    globals()[f"mush{m}y"] = random.randrange(20, CH - 20, 5)
+    globals()[f"mush{m}"] = logs.create_image(globals()[f"mush{x}x"], globals()[f"mush{x}x"], image=seene)
 
 #mario
 ImgM = 10
@@ -103,10 +110,9 @@ master.resizable(False, False)
 
 #vai spēlētājs skatās pa labi
 m = True
-
 #speletāja kustības funkcija
 def playahmove(way):
-    global px, py, playah, marijo, ma, m, punkti, sx, sy
+    global px, py, playah, marijo, ma, m, punkti, sx, sy, i, d
     #vai būs jāmirroro bilde
     w = False
     #pārbauda kurā virziena tas jāpārvieto
@@ -131,11 +137,14 @@ def playahmove(way):
         m = True
         marijo = ImageOps.mirror(marijo)
         ma = ImageTk.PhotoImage(marijo)
-    if px <= sx and px + marijo.size[0] >= sx + mushroom.size[1] and py <= sy and py + marijo.size[1] >= sy + mushroom.size[1]:
-        punkti += 1
-        scoore(punkti)
-        print('l')
-        mushrum()
+    for i in range(1, mushcnt):
+        if px <= globals()[f"mush{i}x"] and px + marijo.size[0] >= globals()[f"mush{i}x"] + mushroom.size[1] and py <= globals()[f"mush{i}y"] and py + marijo.size[1] >= globals()[f"mush{i}y"] + mushroom.size[1]:
+            punkti += 1
+            scoore(punkti)
+            # globals()[f"mush{i}x"] = -1
+            # globals()[f"mush{i}y"] = -1
+            logs.delete(globals()[f"mush{i}"])
+            mushrum(i)
     playah = logs.create_image(px, py, image = ma)
     w = False
 
