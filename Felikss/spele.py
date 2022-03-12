ed6e15d64fb469e920c77170c1e99e43e196a80d
Aqua420 <feliksslacis@gmail.com>
2022-03-12 09:30:29 +0200

    ok

--- Felikss/spele.py
+++ Felikss/spele.py
@@ -26,33 +26,33 @@ a = 100
 b = 100
 direction = None
 
-#def clicked(event):
-#    print("pressed")
-#    e=30
-#    w.create_image(50,400, image = bilde)
+def clicked(event):
+    print("pressed")
+    e=30
+    w.create_image(50,400, image = bilde)
 
 #SPĒLES ELEMENTI (8 krāsu maiņas eleemtni / 5 biezuma maiņas)
-#b = w.create_line(300, 270, 300, 260, width=10, fill='red')
-#w.create_line(300, 240, 300, 230, width=10, fill='yellow')
-#w.create_line(300, 150, 300, 160, width=10, fill='green')
-#w.create_line(300, 200, 300, 190, width=10, fill='black')
-#w.create_line(300, 120, 300, 130, width=10, fill='brown')
-#w.create_line(300, 400, 300, 410, width=10, fill='orange')
+b = w.create_line(300, 270, 300, 260, width=10, fill='red')
+w.create_line(300, 240, 300, 230, width=10, fill='yellow')
+w.create_line(300, 150, 300, 160, width=10, fill='green')
+w.create_line(300, 200, 300, 190, width=10, fill='black')
+w.create_line(300, 120, 300, 130, width=10, fill='brown')
+w.create_line(300, 400, 300, 410, width=10, fill='orange')
 bilde = PhotoImage(file="SPELES_arhivs\semene.ppm")
 pika = PhotoImage(file="SPELES_arhivs\pika.ppm")
-#w.create_image(50,400, image = bilde)
+w.create_image(50,400, image = bilde)
 player = w.create_image(0,0, image = pika)
 w.delete(player)
 
 
 
 
-#def biezums():
-#    global g
-#    buttonBG = w.create_rectangle(100, 0, 200, 30, fill="grey40", outline="grey60")
-#    buttonTXT = w.create_text(150, 15, text= g)
-#    w.tag_bind(buttonBG, "<Button-1>", clicked) ## when the square is clicked runs function "clicked".
-#    w.tag_bind(buttonTXT, "<Button-1>", clicked) ## same, but for the text.
+def biezums():
+    global g
+    buttonBG = w.create_rectangle(100, 0, 200, 30, fill="grey40", outline="grey60")
+    buttonTXT = w.create_text(150, 15, text= g)
+    w.tag_bind(buttonBG, "<Button-1>", clicked) ## when the square is clicked runs function "clicked".
+    w.tag_bind(buttonTXT, "<Button-1>", clicked) ## same, but for the text.
 
 
 #iezums()
@@ -116,15 +116,15 @@ def punkti():
 
 
 
-#objekti = w.find_all()
-#print(objekti)
-#for objekti in objekti:
-#    print(w.coords(objekti))
-#px = w.coords(player)
-#pxx = px[0]
-#pxy = px[1]
-#print(pxx)
-#print(pxy)
+objekti = w.find_all()
+print(objekti)
+for objekti in objekti:
+    print(w.coords(objekti))
+px = w.coords(player)
+pxx = px[0]
+pxy = px[1]
+print(pxx)
+print(pxy)
 
 master.bind_all('<KeyPress>', on_keypress)
 master.bind_all('<KeyRelease>', on_keyrelease)
