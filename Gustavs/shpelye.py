415af4e116f52694eff3e7d77719be41fc81ae1c
gkivlenieks <kivlenieksgustavs@gmail.com>
2022-03-26 10:54:05 +0200

    the begging of mostly unknown existance of start menu

--- Gustavs/shpelye.py
+++ Gustavs/shpelye.py
@@ -5,64 +5,71 @@ from playsound import *
 import threading
 import time
 
+from setuptools import Command
+
 master = Tk()
 punkti = 0
 mushcnt = 10
 
+kkas = False
+
+redy = IntVar()
+
+sterp = 1
+slep = .05
+
 def sound():
     playsound("Gustavs/assets/CoinSE.mp3")
-
 
 # canvas augstums, platums
 CW = 600
 CH = 600
 
-#main logs
-logs = Canvas (
+# main logs
+logs = Canvas(
     master,
     width=CW,
     height=CH
 )
 logs.pack()
 
-# logs.create_rectangle(0, 500, 500, 600, fill='green')
-# logs.create_text(0, 500, 500, 600, text='Sākt')
-
 # player step length
 pstep = 5
 
-#fons
+def mushrum(m):
+    global slep
+    # globals()[f'mush{m}x'] = random.randrange(20, CW - 20, 5)
+    # globals()[f'mush{m}y'] = random.randrange(20, CH - 20, 5)
+    # globals()[f"mush{m}"] = logs.create_image(globals()[f'mush{m}x'], globals()[f'mush{m}y'], image=seene)
+    logs.delete(globals()[f"mush{m}"])
+    globals()[f'mush{m}x'] = -1
+    globals()[f'mush{m}y'] = -1
+    nx = threading.Thread(target=sound, daemon=True)
+    nx.start()
+    slep /= 1.2
+    print(f'slep: {slep}')
+
+titel_bg = logs.create_rectangle(0, 0, CW, 50, fill='grey70')
+titel = logs.create_text(CW // 2, 25, text='Kaut kāds speles nosaukums', font=('Ubuntu Medium', 20))
+sakt_poga = logs.create_rectangle(0, CH - 50, CW, CH, fill='grey70')
+sakt_poga_teksts = logs.create_text(CW // 2, CH - 25, text='Sākt Spēli', font=("Ubuntu Medium", 20))
+
+logs.tag_bind(sakt_poga, "<Button-1>", lambda n: redy.set(1))
+logs.tag_bind(sakt_poga_teksts, "<Button-1>", lambda n: redy.set(1))
+
+logs.wait_variable(redy)
+
+# fons
 grass = PhotoImage(file='Gustavs/assets/graaas.ppm')
-bukgronds = logs.create_image(CW // 2, CH // 2, image= grass)
+bukgronds = logs.create_image(CW // 2, CH // 2, image=grass)
 
-# pa cik pikseļiem playeris pārvietojas
-pstep = 5
-
-#sēne bildes izmēri, pa cik bilde tiks uztaisīta mazāka, tās uzlikšana
+# sēne bildes izmēri, pa cik bilde tiks uztaisīta mazāka, tās uzlikšana
 mushM = 12
 mushroom = Image.open('gustavs/assets/Mushroom.png')
 MushSiz = mushroom.size
 mushroom = mushroom.resize((MushSiz[0] // mushM, MushSiz[1] // mushM))
 seene = ImageTk.PhotoImage(mushroom)
-
-for x in range(mushcnt):
-    globals()[f"mush{x}x"] = random.randrange(20, CW - 20, 5)
-    globals()[f"mush{x}y"] = random.randrange(20, CW - 20, 5)
-    globals()[f"mush{x}"] = logs.create_image(globals()[f'mush{x}x'], globals()[f'mush{x}y'], image= seene)
-    print(f'generated mushroom{x} at {globals()[f"mush{x}x"]}; {globals()[f"mush{x}y"]}')
-
-def mushrum(m):
-    global slep
-    globals()[f'mush{m}x'] = random.randrange(20, CW - 20, 5)
-    globals()[f'mush{m}y'] = random.randrange(20, CH - 20, 5)
-    globals()[f"mush{m}"] = logs.create_image(globals()[f'mush{m}x'], globals()[f'mush{m}y'], image=seene)
-    nx = threading.Thread(target=sound, daemon=True)
-    nx.start()
-    slep /= 1.5
-    print(f'slep: {slep}')
-
-
-#mario
+# mario
 px = CW // 2
 py = CH // 2
 ImgM = 10
@@ -70,9 +77,8 @@ marijo = Image.open('Gustavs/assets/BMario-NoBG.png')
 MarioSiz = marijo.size
 marijo = marijo.resize((MarioSiz[0] // ImgM, MarioSiz[1] // ImgM))
 ma = ImageTk.PhotoImage(marijo)
-playah = logs.create_image(px, py, image = ma)
+playah = logs.create_image(px, py, image=ma)
 print(f'px: {px}; py: {py}')
-
 # kaut kas
 slx = 0
 sly = 0
@@ -80,49 +86,76 @@ slimeeM = 10
 slimee = Image.open('Gustavs/assets/sss.png')
 slimee = slimee.resize((slimee.size[0] // slimeeM, slimee.size[1] // slimeeM))
 slime = ImageTk.PhotoImage(slimee)
-slim = logs.create_image(slx, sly, image = slime)
+slim = logs.create_image(slx, sly, image=slime)
+# score
+score = logs.create_rectangle(0, 0, 100, 50, fill='grey60', outline='black')
+tscore = logs.create_text(50, 25, font=(None, 20), text=punkti)
+
+for x in range(mushcnt):
+    globals()[f"mush{x}x"] = random.randrange(20, CW - 20, 5)
+    globals()[f"mush{x}y"] = random.randrange(20, CW - 20, 5)
+    globals()[f"mush{x}"] = logs.create_image(
+        globals()[f'mush{x}x'], globals()[f'mush{x}y'], image=seene)
+    print(
+        f'generated mushroom{x} at {globals()[f"mush{x}x"]}; {globals()[f"mush{x}y"]}')
+
 
 def smove():
-    global slx, sly, slim, slep
-    sterp = 1
-    slep = .025
+    global slx, sly, slim, slep, sterp, slep
+    j = True
     while 1:
         if slx == px and sly == py:
             print('hehe')
-        if slx < px:
+        if (slx < px or sly > py) and (slx < px or sly > py):
+            if slx < px and j:
+                j = False
+                slx += sterp
+            elif sly < py and j == False:
+                j = True
+                sly += sterp
+            elif slx > px and j:
+                j = False
+                slx -= sterp
+            elif sly > py and j == False:
+                j = True
+                sly -= sterp
+
+        elif slx < px:
             slx += sterp
-        else:
+        elif slx > px:
             slx -= sterp
-        if sly < py:
+        elif sly < py:
             sly += sterp
-        else:
+        elif sly > py:
             sly -= sterp
         logs.delete(slim)
-        slim = logs.create_image(slx, sly, image = slime)
+        slim = logs.create_image(slx, sly, image=slime)
         time.sleep(slep)
 
-slm =  threading.Thread(target=smove, daemon=False)
+
+slm = threading.Thread(target=smove, daemon=False)
 
 slm.start()
 
-#score
-score = logs.create_rectangle(0, 0, 100, 50, fill='grey60', outline='black')
-tscore = logs.create_text(50, 25, font=(None, 20), text=punkti)
+
 def scoore(points):
     global tscore
     logs.delete(tscore)
     tscore = logs.create_text(50, 25, font=(None, 20), text=punkti)
 
-#loga title
+
+# loga title
 master.title("Maaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaarioo")
 
-#resizability
+# resizability
 master.resizable(False, False)
 
-#facing right
+# facing right
 m = True
 
-#player move
+# player move
+
+
 def playahmove(way):
     global px, py, playah, marijo, ma, m
     w = False
@@ -140,17 +173,20 @@ def playahmove(way):
         py += pstep
     logs.delete(playah)
 
-#logs ir/nav resizable
+
+# logs ir/nav resizable
 master.resizable(False, False)
 
-#vai spēlētājs skatās pa labi
+# vai spēlētājs skatās pa labi
 m = True
-#speletāja kustības funkcija
+# speletāja kustības funkcija
+
+
 def playahmove(way):
     global px, py, playah, marijo, ma, m, punkti, i
-    #vai būs jāmirroro bilde
+    # vai būs jāmirroro bilde
     w = False
-    #pārbauda kurā virziena tas jāpārvieto
+    # pārbauda kurā virziena tas jāpārvieto
     if way == 'right':
         mm = True
         px += pstep
@@ -172,7 +208,7 @@ def playahmove(way):
         m = True
         marijo = ImageOps.mirror(marijo)
         ma = ImageTk.PhotoImage(marijo)
-    playah = logs.create_image(px, py, image = ma)
+    playah = logs.create_image(px, py, image=ma)
     for i in range(mushcnt):
         if px <= globals()[f"mush{i}x"] and px + marijo.size[0] >= globals()[f"mush{i}x"] + mushroom.size[1] and py <= globals()[f"mush{i}y"] and py + marijo.size[1] >= globals()[f"mush{i}y"] + mushroom.size[1]:
             punkti += 1
@@ -195,9 +231,11 @@ def on_keypress(event):
     if event.keysym == "d":
         playahmove('right')
 
+
 def on_keyrelease(event):
     global direction
     direction = None
+
 
 master.bind_all('<KeyPress>', on_keypress)
 master.bind_all('<KeyRelease>', on_keyrelease)
