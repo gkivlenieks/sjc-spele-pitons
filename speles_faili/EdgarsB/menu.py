fdb2489c26c38fc497a1824ea8d9060fa35a6ae1
ebajaruns <bajaruns@gmail.com>
2022-03-26 09:05:03 +0200

    fff

--- speles_faili/EdgarsB/menu.py
+++ speles_faili/EdgarsB/menu.py
@@ -1,39 +1,190 @@
-import tkinter
+from calendar import c
+from tkinter import *
+import random
 
-main = tkinter.Tk()
+from numpy import true_divide
+from sklearn.preprocessing import PolynomialFeatures
+canvas_width = 900
+canvas_height = 900
+master = Tk()
 
-mframe = tkinter.Frame(main)
-mframe.pack()
+#mainīgie - kas svarīgi
 
-def clearwin(event=None):
-    '''Clear the main windows frame of all widgets'''
-    for child in mframe.winfo_children():
-        child.destroy()
+direction = None
 
-def win1(event=None):
-    '''Create the main window'''
-    clearwin()
-    b1 = tkinter.Button(mframe, command=win2, text='Window 2')
-    b1.pack()
-    b2 = tkinter.Button(mframe, command=win3, text='Window 3')
-    b2.pack()
+rezultats = 0
 
-def win2(event=None):
-    '''Create the second sub window'''
-    clearwin()
-    entry1 = tkinter.Entry(mframe)
-    entry1.pack()
-    back = tkinter.Button(mframe, command=win1, text='Back')
-    back.pack()
+#Izveidojam spelēs laukumu!!! neaizmirstam komandu .pack()
+logs = Canvas(master, 
+           width=canvas_width, 
+           height=canvas_height)
+master.title("Līnijas spēle")
+logs.pack()
 
-def win3(event=None):
-    '''Create the third sub window'''
-    clearwin()
-    label1 = tkinter.Label(mframe, text='This is the third window!')
-    label1.pack()
-    back = tkinter.Button(mframe, command=win1, text='Back')
-    back.pack()
+#Fona attēla iestatīšana... (svarīgs izmērs - der PNG) (FONS ir globāls mainīgais)
+fons = PhotoImage(file="speles_faili\EdgarsB\mezs_sss.png")
 
-win1()
 
-main.mainloop()
+#Izveidojam spēlētāju (bilde (globāls mainīgais))
+sarkG = PhotoImage(file="speles_faili\EdgarsB\sark_videja.ppm")
+
+# SĒNES (Globāls mainīgais)
+sene = PhotoImage(file="speles_faili\EdgarsB\semene.ppm") 
+
+# UZTAISĪT sēnes (mainot masīva izmēru var veidot vairāk sēnes (arī globāls mainīgais))!!!!
+xkoordinates = []
+ykoordinates = []
+senesst = []
+senes = []
+
+# jāuztaisa ļaunie :) (tiekas uzbruks sarkangalvītei)
+
+#tukšie mainīgie (kas vēlāk tiek "apdzīvoti" (tie vēlāk vajadzīgi, lai izsauktu metodēs))
+uzvarteksts = None
+uzvarteksts1 = None
+
+#atsevišķa funkcija - kas "rada spēlētāju" 
+# 1.funkcija
+def speletajs():
+    global player
+    player = logs.create_image(random.randrange(100, 800, 150),random.randrange(100, 800, 150), image = sarkG) 
+
+#STARTA MENU - sākuma izvēlne (vēl nav uz loga nekas izveidots - tagad izveido "menu, kuru nospiežot tiek izsaukta funkcija (nākamā) nospiests")
+# 2.funkcija
+def SakumaIzvelne():
+    print("Starta menu")
+    global menu1, menu
+    menu1 = logs.create_rectangle(150, 400, 750, 500, fill="white", outline="blue")
+    menu = logs.create_text(450, 450,  font=(None, 50), text="SĀKT SPĒLI")
+    logs.tag_bind(menu1, "<Button-1>", nospiests)
+    logs.tag_bind(menu, "<Button-1>", nospiests)
+
+# funkcija nospiests (pa lielam uzāk spēli - pirmā lieta - nodzēš "izvēlnes pogas" (arī "uzvarastekstu)") un uzliek fona attēlu,
+# iztīra sēņu masīvus un uzģenēr jaunus, tad izsauc 1.funkciju "spēlētājs" )
+# 3.funkcija (ietvars spēlei...)
+
+
+def nospiests(none):
+    logs.delete(menu1)
+    logs.delete(menu)
+    logs.delete(uzvarteksts1)
+    logs.delete(uzvarteksts)
+    print("Starta menu")
+    global menu2, menu3
+    menu2 = logs.create_rectangle(150, 400, 750, 500, fill="white", outline="blue")
+    menu3 = logs.create_text(450, 450,  font=(None, 50), text="TIEŠĀM SĀKT SPĒLI")
+    logs.tag_bind(menu1, "<Button-1>", nospiests2)
+    logs.tag_bind(menu, "<Button-1>", nospiests2)
+
+
+def nospiests2(none):
+    print("nospiests")
+    logs.delete(menu1)
+    logs.delete(menu)
+    logs.delete(uzvarteksts1)
+    logs.delete(uzvarteksts)
+    global xkoordinates, ykoordinates, senes, senesst,sene, fons, sarkG
+    logs.create_image(0,0, image=fons)
+    xkoordinates.clear()
+    ykoordinates.clear()
+    senes.clear()
+    senesst.clear()
+    for i in range(10):
+        xkoordinates.append(random.randrange(100, 800, 150))
+        ykoordinates.append(random.randrange(100, 800, 150))
+        senesst.append(0)  
+        senes.append(i)
+
+    for i in range(10):
+        senes[i] = logs.create_image(xkoordinates[i], ykoordinates[i], image=sene)
+    speletajs()
+    print(senesst)
+    print(senes)
+    print(xkoordinates)
+    print(ykoordinates)    
+
+SakumaIzvelne()
+
+# punktu skaitīšana (kas notiek kad apēd sēni), sev iekša izsauc funkciju "rezultatutablo - katru reizi kad izpildās", iekšā funkcijā punkti
+# ir definēts "spēles rezultāts - kas notiek, kad spēlētājs salasa uzvaras punktus")
+def punkti():
+    global rezultats, senesst, senes, xkoordinates, ykoordinates
+    px = logs.coords(player)
+    pxx = int(px[0])
+    pxy = int(px[1])
+    rezultatutablo()
+
+    for i in range(10):
+        if (xkoordinates[i]-30)<pxx<(xkoordinates[i]+30) and (ykoordinates[i]-30)<pxy<(ykoordinates[i]+30) and senesst[i]==0:
+            logs.delete(senes[i])
+            rezultats = rezultats +1
+            senesst[i]= senesst[i] + 1
+                    
+    if rezultats == 10 :
+        # šis notiek kad "tiek savākti 10 punkti" - parādās "Spēle uzvarēta", kuru nospiežot tiek izsaukta funkcija "nospiests" - principā spēle resetojas!!!
+        global uzvarteksts, uzvarteksts1
+        uzvarteksts1 = logs.create_rectangle(150, 400, 750, 500, fill="white", outline="blue")
+        uzvarteksts = logs.create_text(450, 450,  font=(None, 50), text="Spēle uzvarēta!!!!")
+        logs.tag_bind(uzvarteksts, "<Button-1>", nospiests)
+        logs.tag_bind(uzvarteksts1, "<Button-1>", nospiests)
+        rezultats = 0
+
+# REZULTATU TABLO (lauks, kurā mainās rezultāts (globālie punkti))
+def rezultatutablo():
+    buttonBG = logs.create_rectangle(100, 0, 200, 30, fill="red", outline="grey60")
+    buttonTXT = logs.create_text(150, 15,  font=(None, 25), text=rezultats)
+
+
+
+#KUSTĪBA _ staigājam apkārt... (Šīs 3 tālākas funkcijas vislaik ir aktīvas :) bet faktiski izpildās, tad gad mums ir "globālais player" - funkcija spēlētājs, 
+# funkcija spēlētājs tiek izsaukta "funkcijā nospiests"
+
+def move():
+    global player
+    global x_vel
+    global y_vel
+    global direction
+    if direction is not None:
+        logs.move(player, x_vel,y_vel)
+        punkti()
+        #kustināt vilku... 
+        #after(33,move)
+
+def on_keypress(event):
+    global direction
+    global x_vel
+    global y_vel
+    if event.keysym == "Left":
+        direction = "left"
+        x_vel = -5
+        y_vel = 0
+    if event.keysym == "Right":
+        direction = "right"
+        x_vel = 5
+        y_vel = 0
+    if event.keysym == "Down":
+        direction = "down"
+        x_vel = 0
+        y_vel = 5
+    if event.keysym == "Up":
+        direction = "up"
+        x_vel = 0
+        y_vel = -5
+    
+    move()
+    
+
+def on_keyrelease(event):
+    global direction
+    direction = None
+
+
+
+
+
+
+
+#Master mainloops - izmaiņas un notikumi
+master.bind_all('<KeyPress>', on_keypress)
+master.bind_all('<KeyRelease>', on_keyrelease)
+master.mainloop()
